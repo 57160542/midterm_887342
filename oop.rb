5260dbb81962eae339bdf38984fb744cb06c4964
57160542 <57160542@go.buu.ac.th>
2016-10-08 15:36:49 +0700

    oop code

---
+++ oop.rb
@@ -0,0 +1,20 @@
+ public class Circle
+    PI = 3.14159
+    def initialize (PI  ,radius)
+        
+        @Circle = r
+        @radius = radius
+    end
+    def Circle.Radius
+        @@radius
+    end
+ @@Area = PI * r * radius
+    end
+ @@Cirumference * 2* PI * radius
+    end
+end 
+  Circle.new(10);
+  put"Area is " + @@Area
+  put"Diameter is"+ Circle.Radius
+  put"Circumference is "+ @@Cirumference
+end# midterm_887342
